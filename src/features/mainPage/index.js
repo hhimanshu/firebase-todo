8e85e2a8b520492a21b7d8b21d3dcfba5198abd2
hhimanshu <harit.himanshu@yahoo.in>
2017-10-07 08:00:13 +1300

    Initial Empty page with necessary dependencies

---
+++ src/features/mainPage/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class ToDos extends React.Component {
+    constructor(props) {
+        super()
+    }
+
+    render() {
+        return <h1>Todos here</h1>
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import ToDos from "./features/mainPage/index";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ToDos/>, document.getElementById('root'));
 registerServiceWorker();
